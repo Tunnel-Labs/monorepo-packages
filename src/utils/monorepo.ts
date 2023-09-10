d67369ae260d671e08af6a7a99b7d0b2ff3c2bec
Leon Si <leon@leondreamed.com>
2023-09-10 11:50:55 -0400

    feat: also detect package.json

--- src/utils/monorepo.ts
+++ src/utils/monorepo.ts
@@ -15,19 +15,32 @@ export function getMonorepoDirpath(
 
   let maybeRoot: string | undefined;
   while (curDirectory !== "/") {
-    const pnpmWorkspaceExists = fs.existsSync(
-      path.join(curDirectory, "pnpm-workspace.yaml")
-    );
-    const pnpmLockExists = fs.existsSync(
-      path.join(curDirectory, "pnpm-lock.yaml")
-    );
+    // Check if there is a `package.json` file with the "workspaces" property
+    if (fs.existsSync(path.join(curDirectory, "package.json"))) {
+      const packageJson = JSON.parse(
+        fs.readFileSync(path.join(curDirectory, "package.json"), "utf8")
+      );
 
-    if (pnpmLockExists) {
-      maybeRoot = curDirectory;
-    }
+      if (packageJson.workspaces !== undefined) {
+        return curDirectory;
+      } else {
+        maybeRoot = curDirectory;
+      }
+    } else {
+      const pnpmWorkspaceExists = fs.existsSync(
+        path.join(curDirectory, "pnpm-workspace.yaml")
+      );
+      const pnpmLockExists = fs.existsSync(
+        path.join(curDirectory, "pnpm-lock.yaml")
+      );
 
-    if (pnpmWorkspaceExists) {
-      return curDirectory;
+      if (pnpmLockExists) {
+        maybeRoot = curDirectory;
+      }
+
+      if (pnpmWorkspaceExists) {
+        return curDirectory;
+      }
     }
 
     curDirectory = path.dirname(curDirectory);
